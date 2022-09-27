fb32f1bd6600f40e5a8475364e4f2c183316b934
Dragonspine <ghoul300@naver.com>
2022-09-27 23:48:06 +0900

    feat: Servlet Exception - ErrorPage 작동원리

--- src/main/java/hello/exception/WebServerCustomizer.java
+++ src/main/java/hello/exception/WebServerCustomizer.java
@@ -20,8 +20,13 @@ import org.springframework.stereotype.Component;
 @Component
 public class WebServerCustomizer implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
 
+    /**
+     * WAS(sendError 호출 기록 확인) <- 필터 <- 서블릿 <- 인터셉터 <- 컨트롤러(예외발생)
+     * sendError 호출 기록 확인 후 WAS는 오류 페이지를 출력하기 위해 /error-page/500 를 다시 요청한다.
+     */
     @Override
     public void customize(ConfigurableWebServerFactory factory) {
+
         ErrorPage errorPage404 = new ErrorPage(HttpStatus.NOT_FOUND, "/error-page/404");
         ErrorPage errorPage500 = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/error-page/500");
         // RuntimeException 의 자식 에러도 전부 포함
