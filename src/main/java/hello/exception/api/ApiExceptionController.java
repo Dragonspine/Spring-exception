593fbbac68c914fe050852f274fa0dacb10e5d9b
Dragonspine <ghoul300@naver.com>
2022-09-29 00:06:09 +0900

    feat: API Exception 처리

---
+++ src/main/java/hello/exception/api/ApiExceptionController.java
@@ -0,0 +1,40 @@
+package hello.exception.api;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * packageName    : hello.exception.api
+ * fileName       : ApiExceptionController
+ * author         : kanghyun Kim
+ * date           : 2022/09/28
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2022/09/28        kanghyun Kim      최초 생성
+ */
+@Slf4j
+@RestController
+public class ApiExceptionController {
+
+    @GetMapping("/api/members/{id}")
+    public MemberDto getMember(@PathVariable("id") String id) {
+        if (id.equals("ex")) {
+            throw new RuntimeException("잘못된 사용자");
+        }
+
+        return new MemberDto(id, "hello " + id);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto {
+        private String memberId;
+        private String name;
+    }
+}
